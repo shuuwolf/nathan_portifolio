7a50f9a16c6e868ee82be1e5af0f074dbd38f5ce
ShuuWolf <[email protected]>
2025-09-13 16:03:59 -0300

    feat: Add new project page for The Matheson and update styles

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,7 +2,7 @@ new Glide('.glide', {
     type: 'carousel',
     startAt: 0,
     perView: 1,
-    autoplay: 3500,
+    autoplay: 5000,
     animationDuration: 1500,
     gap: 0,
     hoverpause: false,
