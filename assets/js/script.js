26284d36bc0655b8ff66d8acdf00b90ce07f0464
ShuuWolf <[email protected]>
2025-09-19 12:54:58 -0300

    feat: add mobile menu on the main pages, add responsive design for project list page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+let buttonMobileTrigger = document.getElementById("mobileMenuTrigger");
+buttonMobileTrigger.addEventListener("click", (e) => {
+    if (e.target.classList.contains("mobile-overlay")) {
+        return false;
+    }
+    
+    if(e.currentTarget.classList.contains("mobile-menu-open")){
+        e.currentTarget.classList.remove("mobile-menu-open");
+        e.currentTarget.querySelector('.menu-mobile-icon').innerHTML = `
+        <svg width="32" height="20" viewBox="0 0 34 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.931885 1.28516H32.9319M0.931885 11.467H32.9319M0.931885 21.6488H32.9319" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>`;
+        document.querySelector("body").classList.remove("mobile-menu-open");
+    }else{
+        e.currentTarget.classList.add("mobile-menu-open");
+        e.currentTarget.querySelector('.menu-mobile-icon').innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 14 15" fill="none">
+            <path d="M0.931885 13.4668L12.9319 1.4668M0.931885 1.4668L12.9319 13.4668" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>`;
+        document.querySelector("body").classList.add("mobile-menu-open");
+    }
+})
+
 
 /**
  * Get current page to prevent loading script not in use...
